acfdd3e145fbcfa32b6e54dcbf9b16fc32d153db
Jimmy <43689221+flobbley@users.noreply.github.com>
2019-07-12 10:33:22 -0400

    Update ticTacToe.py

--- Games/ticTacToe.py
+++ Games/ticTacToe.py
@@ -10,16 +10,16 @@ def playerInput(availableGuesses):
         guessPrint = []
         for i in range(1,10):
             if i in availableGuesses:
-                guessPrint.append(str(i))
+                guessPrint.append(str(i)) #creates list of guesses available to the player
             else:
                 guessPrint.append(' ')
         print(' '+guessPrint[6],'|',guessPrint[7],'|',guessPrint[8],'\n-----------\n'
         ,guessPrint[3],'|',guessPrint[4],'|',guessPrint[5],'\n-----------\n'
-        ,guessPrint[0],'|',guessPrint[1],'|',guessPrint[2])
+        ,guessPrint[0],'|',guessPrint[1],'|',guessPrint[2]) #Prints grid of available guesses and associated number input
         print()
         print('available moves are:', availableGuesses)
         pos = int(input())
-        goodGuess = isValid(pos, availableGuesses)
+        goodGuess = isValid(pos, availableGuesses) #makes sure the player guess is in the available guesses
         if goodGuess == True:
             valid = True
             return pos
@@ -33,40 +33,40 @@ def compGuessImpossible(availableGuesses, position, rnd):
     computer will check if it can win, and win if so
     otherwise, computer will play spaces randomly
     """
-    for i in availableGuesses:
+    for i in availableGuesses: #checks if the computer can win by "playing" all the available guesses and checking if they win
         pos_copy = position[:]
         pos_copy[i-1] = 2
         if isWon(pos_copy, 2) == True:
             return i
         
-    for i in availableGuesses:
+    for i in availableGuesses: #checks if the player can win by "playing" all the available guesses and checking if they win
         pos_copy = position[:]
         pos_copy[i-1] = 1
         if isWon(pos_copy, 1) == True:
             return i
 
-    if rnd == 0:
+    if rnd == 0: #if it's the first round, the computer will try to play the middle space, if it's taken it will play a corner space
         if position[4] == 0:
             return 5
         else:
             choose = choice([1,3,7,9])
             return choose
         
-    elif rnd == 1:
-        if position[0]==1 and position[8]==1:
+    elif rnd == 1: # in the second round, the computer will play to block player win (if it can't win)
+        if position[0]==1 and position[8]==1: #checks if opposite corners are taken by the player and if so, plays a side space
             choose = choice([2,4,6,8])
             return choose
         elif position[6] == 1 and position[2] == 1:
             choose = choice([2,4,6,8])
             return choose
-        else:
+        else: #If opposite corners are not taken by player, plays a corner space
             valid = False
             while not valid:
                 choose = choice([1,3,7,9])
-                if choose in availableGuesses:
+                if choose in availableGuesses:#will continue to guess corner spaces until it guesses a corner space that is not occupied
                     return choose
     else:
-        return compGuessEasy(availableGuesses)
+        return compGuessEasy(availableGuesses)#if the above conditions are not met, guesses randomly
         
 def compGuessMedium(availableGuesses, position):
     """
@@ -74,33 +74,33 @@ def compGuessMedium(availableGuesses, position):
     computer will check if it can win, and win if so
     otherwise, computer will play spaces randomly
     """
-    for i in availableGuesses:
+    for i in availableGuesses: #checks if the computer can win by "playing" all the available guesses and checking if they win
         pos_copy = position[:]
         pos_copy[i-1] = 2
         if isWon(pos_copy, 2) == True:
             return i
         
-    for i in availableGuesses:
+    for i in availableGuesses: #checks if the player can win by "playing" all the available guesses and checking if they win
         pos_copy = position[:]
         pos_copy[i-1] = 1
         if isWon(pos_copy, 1) == True:
             return i
     else:
-        i = len(availableGuesses)-1
-        j = randint(0,i)
-        guess = availableGuesses[j]
-        return guess
+        return compGuessEasy(availableGuesses) #if the above conditions are not met, guesses randomly
 
 def compGuessEasy(availableGuesses):
     """
     computer plays spaces randomly, won't even play space to win if it has the opportunity
     """
-    i = len(availableGuesses)-1
+    i = len(availableGuesses)-1 #returns a random value from the list of available guesses
     j = randint(0,i)
     guess = availableGuesses[j]
     return guess
 
 def guessRemover(guess, availableGuesses):
+    """
+    removes guessed spaces from the list of available guesses
+    """
     availableGuesses.remove(guess)
     print()
     return availableGuesses
@@ -109,27 +109,30 @@ def isWon(pos, pla):
     """
     checks if spaces needed to win are occupied by the specified player (pla)
     """
-    if pos[0] == pla and pos[1]==pla and pos[2]==pla:
+    if pos[0] == pla and pos[1]==pla and pos[2]==pla: #checks bottom row
         won = True
-    elif pos[3] ==pla and pos[4]==pla and pos[5]==pla:
+    elif pos[3] ==pla and pos[4]==pla and pos[5]==pla: #checks middle row
         won = True
-    elif pos[6] ==pla and pos[7]==pla and pos[8]==pla:
+    elif pos[6] ==pla and pos[7]==pla and pos[8]==pla: #checks top row
         won = True
-    elif pos[0] ==pla and pos[3]==pla and pos[6]==pla:
+    elif pos[0] ==pla and pos[3]==pla and pos[6]==pla: #checks left column
         won = True
-    elif pos[1]==pla and pos[4]==pla and pos[7]==pla:
+    elif pos[1]==pla and pos[4]==pla and pos[7]==pla: #checks middle column
         won = True
-    elif pos[2]==pla and pos[5]==pla and pos[8]==pla:
+    elif pos[2]==pla and pos[5]==pla and pos[8]==pla: #checks right column
         won = True
-    elif pos[0]==pla and pos[4] ==pla and pos[8]==pla:
+    elif pos[0]==pla and pos[4] ==pla and pos[8]==pla: #checks forward diagonal
         won = True
-    elif pos[6]==pla and pos[4] == pla and pos[2]==pla:
+    elif pos[6]==pla and pos[4] == pla and pos[2]==pla: #checks backward diagonal
         won = True
     else:
         won = False
     return won
 
 def isValid(guess, availableGuesses):
+    """
+    determines if a guess is in the available guesses
+    """
     if guess in availableGuesses:
         return True
     else:
@@ -140,19 +143,22 @@ def ticPrint(position):
     prints the tic-tac-toe board as guesses are made
     """
     position_mark = []
-    for i in position:
-        if i == 0:
+    for i in position: #fills the "position_mark" list with the appropriate marker depending on who occupies a space
+        if i == 0: #0 represents an unoccupied space
             position_mark.append(' ')
-        elif i == 1:
+        elif i == 1: #1 represents a space occupied by the player
             position_mark.append('X')
-        elif i == 2:
+        elif i == 2: #2 represents a space occupied by the computer
             position_mark.append('O')
-    print(' '+position_mark[6],'|',position_mark[7],'|',position_mark[8],'\n-----------\n'
+    print(' '+position_mark[6],'|',position_mark[7],'|',position_mark[8],'\n-----------\n' #Prints the Tic Tac Toe board with the marked spaces
           ,position_mark[3],'|',position_mark[4],'|',position_mark[5],'\n-----------\n'
           ,position_mark[0],'|',position_mark[1],'|',position_mark[2])
     return ''
 
 def diffSelect():
+    """
+    Allows the player to select the difficulty they would like to play
+    """
     valid = False
     while not valid:
         print("\nWhat difficulty would you like?")
@@ -167,6 +173,9 @@ def diffSelect():
             print('Invalid Option')
 
 def playOrder():
+    """
+    Determines the play order
+    """
     valid = False
     while not valid:
         print("Who goes first?")
@@ -177,50 +186,52 @@ def playOrder():
             return 1
 
 def playerTurn(availableGuesses, position):
-    #player turn
-    pos1 = playerInput(availableGuesses)
-    position[pos1-1] = 1
-    availableGuesses = guessRemover(pos1, availableGuesses)
-    won = isWon(position, 1)
+    """
+    player turn
+    """
+    pos1 = playerInput(availableGuesses) #asks the player for a guess
+    position[pos1-1] = 1 #places a player marker in the space chosen
+    availableGuesses = guessRemover(pos1, availableGuesses) #removes guessed space from available guesses
+    won = isWon(position, 1) #checks if the player won in this round
     print('Your move:')
     print(ticPrint(position))
     return availableGuesses, position, won
 
 def computerTurn(availableGuesses, position, difficulty, rnd):
-    #computer turn
+    """
+    Computer turn, plays according to difficulty selected
+    """
     print('Computer plays:')
     if difficulty == 2:
         pos2 = compGuessMedium(availableGuesses, position)
     elif difficulty == 1:
         pos2 = compGuessEasy(availableGuesses)
-    elif difficulty == 3:
+    elif difficulty == 3:      
         pos2 = compGuessImpossible(availableGuesses, position, rnd)
+        rnd+=1
     position[pos2-1] = 2
     availableGuesses = guessRemover(pos2, availableGuesses)
     won = isWon(position, 2)
     print(ticPrint(position))
-    return availableGuesses, position, won
+    return availableGuesses, position, won, rnd
 
 def ticTacToe():
-    playing = True
-    while playing:
+    while True:
         print('+++++++++++++++++++++++++++++++')
         print()
         print('Welcome to Tic-Tac-Toe!')
         print()
         print('+++++++++++++++++++++++++++++++')
-        availableGuesses = [1,2,3,4,5,6,7,8,9]
-        position = [0,0,0,0,0,0,0,0,0]
+        availableGuesses = [1,2,3,4,5,6,7,8,9] #fills the available guess positions
+        position = [0,0,0,0,0,0,0,0,0] #blanks the board
         difficulty = diffSelect()
         print(ticPrint(position))
-        order = playOrder()
+        order = playOrder() #determines play order, 0 is player first, 1 is computer first
         rnd = 0
-        availableGuesses = [1,2,3,4,5,6,7,8,9]
-        position = [0,0,0,0,0,0,0,0,0]
         won = False
         while not won:
             
-            if order == 0:                
+            if order == 0: #when "order" is 0 it is the player's turn             
                 availableGuesses, position, won = playerTurn(availableGuesses, position)
                 if won == True:
                     print('You win!\n')
@@ -229,8 +240,8 @@ def ticTacToe():
                     print('Stalemate!\n')
                     break
                 
-            else:
-                availableGuesses, position, won = computerTurn(availableGuesses, position, difficulty, rnd)
+            else: #when "order" is not 0 it is the computer's turn
+                availableGuesses, position, won, rnd = computerTurn(availableGuesses, position, difficulty, rnd)
                 if won == True:
                     print('The computer wins this round!\n')
                     break
@@ -238,18 +249,15 @@ def ticTacToe():
                     print('Stalemate!\n')
                     break
 
-            order = 1 - order
-            rnd+=1
+            order = 1 - order #changes the turn
             
             input('Press ENTER to continue\n')
                 
-        print('Would you like to play again? y/n')
+        print('Would you like to play again? y/n') #play again prompt
         game = input()
         if game == 'n':
-            playing = False
             print('\nGoodbye!')
             input()
+            break
     return ''
-
-
 
