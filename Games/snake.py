0b57d8fe9afd08bfa75011645ab8f59158596436
Jimmy <43689221+flobbley@users.noreply.github.com>
2020-01-23 13:00:36 -0500

    Add files via upload

--- Games/snake.py
+++ Games/snake.py
@@ -7,18 +7,22 @@ if syst == 'nt':
 else:
     clearVar = "clear"
     
-def gridSet(x, y, positions, apple):
+def gridSet(x, y, positions, apple, blocked):
     """
     Draws grid with all relevant characters.
     Starts at the top of the board and draws the board row by row using the coordinates "right" and "down"
     At each coordinate checks what mark should be printed and adds it to a line string which is printed for each row 
     """
-    appleChar = 'x'
+    appleChar = 'Ó'
     spaceChar = ' '
-    topChar = '-'
-    sideChar = '|'
-    char = 'O'
+    bottomChar = '▀'
+    topChar = '▄'
+    sideChar = '█'
+    char = '#'
+    blockedChar = '█'
     down = 0
+    lineBreak = '\n'
+    lines = []
     print(topChar*(x+2)) #adds top and bottom border
     for height in range(y): #begins building grid by row
         right = 0
@@ -32,10 +36,12 @@ def gridSet(x, y, positions, apple):
                 line+=char
             elif [right, down] == apple: #adds symbol for apple if coord is apple
                 line+=appleChar
+            elif [right, down] in blocked:
+                line+=blockedChar
             else:
                 line+=spaceChar #otherwise adds empy char
         print(line)
-    print(topChar*(x+2))
+    print(bottomChar*(x+2))
 
 def appleRandom(x,y):
     """
@@ -46,17 +52,17 @@ def appleRandom(x,y):
     apple = [appleX, appleY]
     return apple
     
-def applePlacer(x,y,positions):
+def applePlacer(x,y,positions, blocked):
     """
     Places a new apple on the grid, tries to randomly place apples
     until the apple is in an unoccupied square
     """
     while True:
         apple = appleRandom(x,y)
-        if apple not in positions:
+        if apple not in positions and apple not in blocked:
             return apple
 
-def moveChar(x,y,positions, direction, apple, score):
+def moveChar(x,y,positions, direction, apple, score, blocked):
     """
     Moves the snake around the grid and keeps track of score
     """
@@ -83,11 +89,13 @@ def moveChar(x,y,positions, direction, apple, score):
         return False
     if pos1 in positions:#checks if the front coordinate hit the snake
         return False
+    if pos1 in blocked: #checks if front coordinate hit a blocked section
+        return False
     else:
         positions.append(pos1)#moves the front position permanently 
         if pos1 == apple: #checks if snake ate an apple, snake is lengthened by not removing back coordinate
             score += 100
-            apple = applePlacer(x,y,positions)#places new apple
+            apple = applePlacer(x,y,positions, blocked)#places new apple
         else:
             positions = positions[1:]#if an apple is not eaten, the snake "moves" by removing the back coordinate after a new front coordinate is added
     return positions, direction, apple, score
@@ -110,22 +118,54 @@ def directionChanger(direction):
             direction = 'right'
     return direction
 
+def levelSelect():
+    """
+    gives blocked spaced based on the player's level selection
+    """
+
+    level0 = []
+    level1 = [[5,5],[5,6],[5, 4],[5,7],[5, 8],[5, 3],[14, 3],[14, 4],[14,5],[14,6],[14,7],[14,8]]
+    levels = [level0, level1]
+    while True:
+        print('Which level would you like to play?')
+        level = input('1. Open\n2. Parallel\n')
+        try:
+            a = int(level)
+            try:
+                return levels[a-1]
+            except IndexError:
+                print('Invalid Selection')
+                input()
+                os.system(clearVar)
+        except ValueError:
+            print('Invalid Selection')
+            input()
+            os.system(clearVar)
+        
+
 def snake():
     """
     Main Game
     """
+    hiScore = 0
     while True: #Start Game Loop, starts new games until the player wants to quit
         os.system(clearVar)
-
+        print('+++++++++++++++++++++++++++++++')
+        print()
+        print('        ~~~~Snake!~~~~')
+        print()
+        print('+++++++++++++++++++++++++++++++')
         x, y = 18, 10 #sets board size
 
         pos1 = [x//2, y//2] #sets inital snake positions
         pos2 = [pos1[0]+1, pos1[1]]
         pos3 = [pos1[0]+2, pos1[1]]
         positions = [pos3, pos2, pos1]
-        
-        apple = applePlacer(x,y,positions) #places starting apple
-        gridSet(x, y, positions, apple) #sets up initial grid
+        blocked = levelSelect()
+
+        os.system(clearVar)
+        apple = applePlacer(x,y,positions, blocked) #places starting apple
+        gridSet(x, y, positions, apple, blocked) #sets up initial grid
 
         score = 0
         delay = 150
@@ -149,14 +189,17 @@ def snake():
                 checked = False
                 direction = directionTemp
                 try: #if moveChar returns False, will give a TypeError. This means a Game Over has happened
-                    positions, direction, apple, score = moveChar(x, y, positions, direction, apple, score)
+                    positions, direction, apple, score = moveChar(x, y, positions, direction, apple, score, blocked)
                     os.system(clearVar)
-                    gridSet(x, y, positions, apple)
+                    gridSet(x, y, positions, apple, blocked)
                     print('Score:', score)
+                    print('High Score:', hiScore)
                     startTurn = int(round(time.time()*1000)) #sets the start time for the turn
                 except TypeError:
                     print('Game Over!')
-                    input()
+                    if score >hiScore:
+                        hiScore = score
+                        print('New High Score!')
                     break
 
         print('Would you like to play again? y/n') #prompt player to play again if they lose
